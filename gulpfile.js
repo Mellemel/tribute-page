1bc32bf482f4d5b8647a4b6cecd6716faa0ee7b9
Melvin Canela <M.Canela92@outlook.com>
2016-07-29 18:19:15 -0400

    added gulp task to move font folder and gulp task to minify js

--- gulpfile.js
+++ gulpfile.js
@@ -6,13 +6,18 @@ var gulp = require('gulp'),
     htmlMin = require('gulp-htmlmin'),
     cleanCSS = require('gulp-clean-css'),
     imageMin = require('gulp-imagemin'),
-    sourceMaps = require('gulp-sourcemaps'),
+    jsmin = require('gulp-jsmin'),
+    rename = require('gulp-rename'),
     browserSync = require('browser-sync').create()
 
 gulp.task('serve', () => {
     browserSync.init({
+        files: './builds/production/**',
         server: {
             baseDir: './builds/production/'
+        },
+        watchOptions: {
+            usePolling: true
         }
     })
 
@@ -20,10 +25,12 @@ gulp.task('serve', () => {
         .on('change', browserSync.reload)
 })
 
-gulp.task('watch', ['jade', 'sass', 'imageMin'], () => {
+gulp.task('watch', ['jade', 'sass', 'imageMin', 'jsmin', 'movefont'], () => {
     gulp.watch('./src/*.jade', ['jade'])
     gulp.watch('./src/*.scss', ['sass'])
     gulp.watch('./src/images/*', ['imageMin'])
+    gulp.watch('./src/js/*.js', ['jsmin'])
+    gulp.watch('./src/fonts/*', ['movefont'])
 })
 
 gulp.task('jade', () => {
@@ -36,22 +43,34 @@ gulp.task('jade', () => {
 })
 
 gulp.task('sass', () => {
-    return gulp.src('./src/*.scss')
-        .pipe(sourceMaps.init())
+    return gulp.src('./src/scss/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(sourceMaps.write('./builds/development'))
-        .pipe(gulp.dest('./builds/development'))
+        .pipe(gulp.dest('./builds/development/css'))
         .pipe(cleanCSS({ debug: true }, (details) => {
             console.log(details.name + ': ' + details.stats.originalSize)
             console.log(details.name + ': ' + details.stats.minifiedSize)
         }))
-        .pipe(gulp.dest('./builds/production'))
+        .pipe(gulp.dest('./builds/production/css'))
         .pipe(browserSync.stream())
 })
 
 gulp.task('imageMin', () => {
-    gulp.src('./src/images/*')
+    return gulp.src('./src/images/*')
         .pipe(imageMin({ verbose: true }))
         .pipe(gulp.dest('./builds/development/images'))
         .pipe(gulp.dest('./builds/production/images'))
+})
+
+gulp.task('jsmin', () => {
+    return gulp.src('./src/js/*.js')
+        .pipe(gulp.dest('./builds/development/js'))  
+        .pipe(jsmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('./builds/production/js'))
+})
+
+gulp.task('movefont', ()=>{
+    return gulp.src('./src/fonts/*')
+        .pipe(gulp.dest('./builds/development/font'))
+        .pipe(gulp.dest('./builds/production/font'))
 })
